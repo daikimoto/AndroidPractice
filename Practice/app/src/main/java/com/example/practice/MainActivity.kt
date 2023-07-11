841ddfc2d9c0824cf7c931e016718014993c9ba0
本山大輝 <daiki120902@icloud.com>
2023-07-11 23:07:28 +0900

    MainActivityからSpinnerActivityへの遷移を実装

--- Practice/app/src/main/java/com/example/practice/MainActivity.kt
+++ Practice/app/src/main/java/com/example/practice/MainActivity.kt
@@ -14,10 +14,10 @@ class MainActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        /*
+
         binding.toSpinnerButton.setOnClickListener {
             val intent = Intent(application,SpinnerActivity::class.java)
             startActivity(intent)
-        }*/
+        }
     }
 }
