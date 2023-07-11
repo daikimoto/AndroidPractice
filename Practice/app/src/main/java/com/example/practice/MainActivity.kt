4ef4f2f489e969519627bcd4174963597d939b36
本山大輝 <daiki120902@icloud.com>
2023-07-11 23:05:16 +0900

    MainActivityにSpinnerActivityへの遷移用のボタンを配置。（未接続）

--- Practice/app/src/main/java/com/example/practice/MainActivity.kt
+++ Practice/app/src/main/java/com/example/practice/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.practice
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import com.example.practice.databinding.ActivityMainBinding
+
 
 class MainActivity: AppCompatActivity() {
 
+    private lateinit var  binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        /*
+        binding.toSpinnerButton.setOnClickListener {
+            val intent = Intent(application,SpinnerActivity::class.java)
+            startActivity(intent)
+        }*/
     }
 }
