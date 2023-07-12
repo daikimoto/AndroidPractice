0a3848be34867cb97c8fd6553d5d28e73a00862e
本山大輝 <daiki120902@icloud.com>
2023-07-12 20:30:46 +0900

    SpinnerActivityをViewBindingにリファクタリング

--- Practice/app/src/main/java/com/example/practice/SpinnerActivity.kt
+++ Practice/app/src/main/java/com/example/practice/SpinnerActivity.kt
@@ -11,21 +11,25 @@ import com.example.practice.databinding.ActivitySpinnerBinding
 
 class SpinnerActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySpinnerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_spinner)
 
-        var user_name_tv = findViewById<TextView>(R.id.user_name_tv)
-        var spinner = findViewById<Spinner>(R.id.spinner)
+        binding = ActivitySpinnerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var user_name_tv = binding.userNameTv
+        var spinner = binding.spinner
 
         //xmlファイルからアイテムの配列を取得
         val items = resources.getStringArray(R.array.user_items)
 
         //アダプターにアイテム配列を設定
-        val Adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+        val spinner_adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
 
         //スピナーにアダプターを設定
-        spinner.adapter = Adapter
+        spinner.adapter = spinner_adapter
 
         //スピナーのセレクトイベント設定
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
@@ -41,6 +45,7 @@ class SpinnerActivity : AppCompatActivity() {
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
+            //
             }
         }
 
